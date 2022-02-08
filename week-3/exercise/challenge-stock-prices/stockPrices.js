9f81f1db7d72a323ba847d9331cf8638bb16422c
Meke Finau <lonifinau.09@gmail.com>
2022-02-08 09:53:15 -0700

    update

---
+++ week-3/exercise/challenge-stock-prices/stockPrices.js
@@ -0,0 +1,26 @@
+sell = [15, 10, 20, 22, 1, 9]
+// 12
+
+const best = max => {
+
+    for(let i=0; i<sell.length; i++)
+
+    for(let j=0; j<sell.length -1; i++)
+
+    if( [i] - [j] = (best.max) )
+}
+
+
+// function best(arr) {
+//     let max = Math.max(...arr);
+//     let maxindex = arr.indexOf(max);
+//     const newValues = [];
+//     for(let i = 0; i < maxindex; i++) {
+//      newValues.push(max - arr[i])
+//     }
+//     const maxProfit = Math.max(...newValues);
+//     if (maxProfit > 0) {
+//      return `Max Profit $${maxProfit} - buy at $${max - maxProfit}, sell at $${max}`
+//     }
+//     return `0`
+//    }
